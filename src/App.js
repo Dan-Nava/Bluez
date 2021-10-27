a1aa21464dce76eeb8dff490bd7584624b784b3c
Jiaxing Leo Li <leojiaxing.li@mail.utoronto.ca>
2021-10-27 16:05:10 -0400

    Start on landing/login component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+import React from 'react';
+import Home from "./react-components/Landing/landing";
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit src/App.js and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {}
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <BrowserRouter>
+                        <Switch>
+                            <Route exact path='/' render={() =>
+                                (<Home appState={this.state}/>)}/>
+                        </Switch>
+                    </BrowserRouter>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/react-components/Landing/landing.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./landing.css";
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="home">
+                <p>
+                    Landing page.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Home;
