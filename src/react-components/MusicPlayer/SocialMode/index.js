27fadd090397e97a64c77a017e7f5ec5e031b9aa
Jiaxing Leo Li <leojiaxing.li@mail.utoronto.ca>
2021-11-04 19:18:57 -0400

    Create spectrum drawer

--- src/react-components/MusicPlayer/SocialMode/index.js
+++ src/react-components/MusicPlayer/SocialMode/index.js
@@ -1,14 +1,82 @@
 import React from 'react';
+import './styles.css'
 
-// This component is the component that will display the screen for LyricMode. 
 class SocialMode extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = this.props.state;
+        this.canvasWidth = 320;
+        this.canvasHeight = 200;
+        this.spectrumPadding = 3;
+        this.canvas = React.createRef();
+    }
 
-	render() {
-		return (
-			<div>
-			</div>
-		)
-	}
+    setupAnalyzer() {
+        this.audioContext = this.audioContext || new AudioContext();
+        this.source = this.source || this.audioContext.createMediaElementSource(this.props.audio_object);
+        this.analyser = this.analyser || this.audioContext.createAnalyser();
+        this.analyser.fftSize = 64
+        this.source.connect(this.audioContext.destination);
+        this.source.connect(this.analyser);
+    }
+
+    getFrequencyAmplitude() {
+        const bufferLength = this.analyser.frequencyBinCount;
+        const amplitudeArray = new Uint8Array(bufferLength);
+        this.analyser.getByteFrequencyData(amplitudeArray)
+        return this.normalize(amplitudeArray)
+    }
+
+    normalize(arr) {
+        let max = arr.reduce(function (a, b) {
+            return Math.max(a, b);
+        }, 0);
+        let results = [...Array(arr.length).keys()]
+        for (let i = 0; i < arr.length; i++) {
+            results[i] = arr[i] / max;
+        }
+        return results
+    }
+
+    drawSpectrum(amplitudeData) {
+        const ctx = this.canvas.current.getContext("2d");
+        ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+        ctx.fillStyle = "#FF0000";
+        let w = this.canvasWidth / amplitudeData.length - this.spectrumPadding;
+        for (let i = 0; i < amplitudeData.length; i++) {
+            ctx.fillRect((w + this.spectrumPadding) * i, this.canvasHeight - amplitudeData[i] * this.canvasHeight, w, amplitudeData[i] * this.canvasHeight);
+        }
+    }
+
+    updateSpectrum() {
+        this.drawSpectrum(this.getFrequencyAmplitude())
+        let t = this;
+        this.audioAnimation = requestAnimationFrame(function () {
+            t.updateSpectrum();
+        })
+    }
+
+    stateChangeHandler(playState) {
+        if (this.source) {
+            this.source.disconnect();
+        }
+        cancelAnimationFrame(this.audioAnimation);
+
+        this.setupAnalyzer();
+        this.setState({playState: playState})
+        let t = this;
+        this.audioAnimation = requestAnimationFrame(function () {
+            t.updateSpectrum();
+        })
+    }
+
+    render() {
+        return (
+            <div className="socialContainer">
+                <canvas className="waveCanvas" ref={this.canvas} width={this.canvasWidth} height={this.canvasHeight}/>
+            </div>
+        );
+    }
 }
 
 export default SocialMode;
