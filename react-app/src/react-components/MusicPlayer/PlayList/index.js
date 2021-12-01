d3032841b84d03af72e256f5512e01e842b9687f
Dan-Nava <daniel.nava@mail.utoronto.ca>
2021-11-30 23:44:43 -0500

    basic playlist functionality implemented, search bar implemented

--- react-app/src/react-components/MusicPlayer/PlayList/index.js
+++ react-app/src/react-components/MusicPlayer/PlayList/index.js
@@ -5,29 +5,51 @@ import Divider from '@mui/material/Divider'
 import Avatar from '@mui/material/Avatar'
 import Grid from '@mui/material/Grid'
 import ListItemButton from "@mui/material/ListItemButton";
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import AlbumIcon from '@mui/icons-material/Album'
 
 import './styles.css'
 
 export default class PlayList extends React.Component {
-
+    
     constructor(props) {
         super(props);
-        this.playList = this.props.state.playList;
+        this.state = {
+            playList: [],
+        }
     }
 
-    playListPanel(songName) {
+    songList = this.props.state.playList;
+
+    //'extras' only relevant to search popup, 
+    // it lets autocomplete component know that an element has been clicked
+    playListPanel(songName, searching, extras) {
         return (
             <div key={Math.random()}>
                 <Divider/>
-                <ListItem key={Math.random()}>
-                    <ListItemButton onClick={(e) => (this.props.setSong(songName))} key={Math.random()}>
+                <ListItem key={Math.random()} {...extras}>
+                    <ListItemButton onClick={(e) => {
+                        if (searching) {
+                            this.addToPlaylist(songName);
+                        } else {
+                            this.props.setSong(songName);
+                        }  
+                    }} 
+                    key={Math.random()}
+                    >
                         <Grid container direction='row' spacing={1}>
                             <Grid item>
-                                <Avatar></Avatar>
+                                <Avatar variant='square'><AlbumIcon/></Avatar>
                             </Grid>
                             <Grid item>
-                                <Grid item className='playlist-song-name'>
-                                    {songName}
+                                <Grid container direction='column' spacing={1}>
+                                    <Grid item className='playlist-cell-text'>
+                                        {songName}
+                                    </Grid>
+                                    <Grid item className='playlist-cell-text'>
+                                        {'artist'}
+                                    </Grid>
                                 </Grid>
                             </Grid>
                         </Grid>
@@ -37,22 +59,49 @@ export default class PlayList extends React.Component {
         )
     }
 
-    playListRendering(list) {
-        const plist = []
-        for (let i = 0; i < list.length; i++) {
-            plist.push(this.playListPanel(list[i]));
+    renderPlaylist() {
+        const plist = [];
+        for (let i = 0; i < this.state.playList.length; i++) {
+            plist.push(this.playListPanel(this.state.playList[i], false));
         }
         return (plist)
     }
 
+    addToPlaylist(songName) {
+        this.setState({
+            playList: [...this.state.playList, songName]
+        });
+    }
+
+    renderPlaylistSearch() {
+        return (
+            <div id='playlist-search-div'>
+                <Autocomplete
+                    id="playlist-search-autocomplete"
+                    options={this.songList}
+                    freeSolo
+                    size='small'
+                    //changes value shown in searchbox after selection
+                    // and is the value the user must search for to find the song
+                    getOptionLabel = {(option) => (option)} 
+                    renderInput={(params) => <TextField {...params} label="Search Music" />}
+                    renderOption={(props, song) => (this.playListPanel(song, true, props))}  
+                />
+            </div>
+        )
+    }
+
     render() {
         return (
-            <div id='play-list-container'>
-                <p><b>Playlist</b></p>
-                <List id='play-list' variant='outlined'>
-                    {this.playListRendering(this.playList)}
-                </List>
-            </div>
+            <>  
+                {this.renderPlaylistSearch()}
+                <div id='play-list-container'>
+                    <p><b>Playlist</b></p>
+                    <List id='play-list' variant='outlined'>
+                        {this.renderPlaylist()}
+                    </List>
+                </div>
+            </>
         )
     }
 }
